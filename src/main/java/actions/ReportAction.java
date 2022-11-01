95005a7aec85505a8684aa8f04a934334e7182ea
katsuaki.niihrara <ktyhym@gmail.com>
2022-11-01 15:55:31 +0900

    Reportモデル、ReportViewモデル作成

---
+++ src/main/java/actions/ReportAction.java
@@ -0,0 +1,15 @@
+package actions;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+public class ReportAction extends ActionBase {
+
+    @Override
+    public void process() throws ServletException, IOException {
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
+
+}
